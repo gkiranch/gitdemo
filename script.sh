eb141a01707732ec53276fa45a34b4082f8eb065
Kiran <kiran@MegaMind.localdomain>
2022-05-24 15:41:02 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello world"
+echo "###########################" & "##################"
+echo "What is your name"
+read name
+echo "Hello $name, how are you; hope you are well"
+
