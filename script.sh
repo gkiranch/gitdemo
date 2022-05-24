8afb5f001afd80b5dd4d282f945498df981e023a
Kiran <kiran@MegaMind.localdomain>
2022-05-24 18:13:13 +0530

    deleted files

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hello world"
-echo "###########################" & "##################"
-echo "What is your name"
-read name
-echo "Hello $name, how are you; hope you are well"
-
